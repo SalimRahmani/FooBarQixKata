41e8b15ae8f6ccc8f910381cc8dfda31f1e49b55
Salim Rahmani <SalimRahmani@outlook.com>
2018-03-06 13:55:55 +0000

    Initial commit

---
+++ FooBarQixKata/FooBarQix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBarQixKata
+{
+    public class FooBarQix
+    {
+        public string Transform(int number)
+        {
+            string result = "";
+            return string.IsNullOrEmpty(result) ? number.ToString(CultureInfo.InvariantCulture) : result;
+        }
+    }
+}
---
+++ FooBarQixKataTest/FooBarQixTest.cs
@@ -0,0 +1,17 @@
+﻿using FooBarQixKata;
+using NUnit.Framework;
+
+namespace FooBarQixKataTest
+{
+    public class FooBarQixTest
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(11, "11")]
+        public void should_return_same_number_if_not_divisible_by_3_5_7_or_contains_3_5_7(int number, string expected)
+        {
+            FooBarQix fooBarQix = new FooBarQix();
+            Assert.AreEqual(fooBarQix.Transform(number), expected);
+        }
+    }
+}
